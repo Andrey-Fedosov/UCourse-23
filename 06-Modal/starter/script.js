7ab697f5993a9901194ccb77380f87ee21e5f274
Andrey-Fedosov <andrefedosov@gmail.com>
2023-05-02 23:09:07 +0300

    feature: add task from codecamp

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -99,31 +99,75 @@ document.addEventListener('keydown', function (keyDowned) {
   }
 }); */
 
-const showModalBtn = document.querySelectorAll('.show-modal');
-const modalWindowElement = document.querySelector('.modal');
-const closeModalBtn = document.querySelector('.close-modal');
-const overlayElement = document.querySelector('.overlay');
+// const showModalBtn = document.querySelectorAll('.show-modal');
+// const modalWindowElement = document.querySelector('.modal');
+// const closeModalBtn = document.querySelector('.close-modal');
+// const overlayElement = document.querySelector('.overlay');
 
-const closeModal = function () {
-  modalWindowElement.classList.add('hidden');
-  overlayElement.classList.add('hidden');
+// const closeModal = function () {
+//   modalWindowElement.classList.add('hidden');
+//   overlayElement.classList.add('hidden');
+// };
+
+// for (let j = 0; j < showModalBtn.length; j++) {
+//   showModalBtn[j].addEventListener('click', function () {
+//     modalWindowElement.classList.remove('hidden');
+//     overlayElement.classList.remove('hidden');
+//   });
+// }
+
+// closeModalBtn.addEventListener('click', closeModal);
+
+// overlayElement.addEventListener('click', closeModal);
+
+// document.addEventListener('keydown', function (keyDowned) {
+//   if (keyDowned.key === 'Escape') {
+//     if (!modalWindowElement.classList.contains('hidden')) {
+//       closeModal();
+//     }
+//   }
+// });
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name'],
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette'],
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: [],
+  },
+  5439: {
+    albumTitle: 'ABBA Gold',
+  },
 };
 
-for (let j = 0; j < showModalBtn.length; j++) {
-  showModalBtn[j].addEventListener('click', function () {
-    modalWindowElement.classList.remove('hidden');
-    overlayElement.classList.remove('hidden');
-  });
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (value == '') {
+    delete records[id][prop];
+  } else if (prop !== 'tracks' && value != '') {
+    records[id][prop] = value;
+  } else if (prop === 'tracks' && value !== '') {
+    if (!records[id].hasOwnProperty(prop)) {
+      records[id][prop] = [];
+    }
+    records[id][prop].push(value);
+  }
+  console.log(records);
+  return records;
 }
 
-closeModalBtn.addEventListener('click', closeModal);
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 
-overlayElement.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (keyDowned) {
-  if (keyDowned.key === 'Escape') {
-    if (!modalWindowElement.classList.contains('hidden')) {
-      closeModal();
-    }
-  }
-});
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+updateRecords(recordCollection, 5439, 'tracks', 'Take a Chance on Me');
+updateRecords(recordCollection, 1245, 'tracks', 'Addicted to Love');
+updateRecords(recordCollection, 2468, 'tracks', 'Free');
