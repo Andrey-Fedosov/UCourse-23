b8be083d5b27cb7789c77d264fda68808b892500
Andrey-Fedosov <andrefedosov@gmail.com>
2023-06-09 06:51:41 +0300

    add exercise 2 abou closures

--- 10-functions/starter/script.js
+++ 10-functions/starter/script.js
@@ -1,6 +1,23 @@
 'use strict';
 ////////// 08.06.2006
 
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`here are 3 groups, each  with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`We will start boarding in ${wait} seconds`);
+};
+const perGroup = 1000;
+boardPassengers(45, 2);
+
+// setTimeout(function () {
+//   console.log('TIMER', 5000);
+// });
+
 let f;
 
 const g = function () {
@@ -29,297 +46,297 @@ console.dir(f);
 // console.dir(f);
 //////////closure
 
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`there are ${passengerCount} passengers`);
-  };
-};
-
-const addPassenger = secureBooking();
-
-addPassenger();
-addPassenger();
-addPassenger();
-
-console.dir(addPassenger);
-
-/////// immideately invoked function expression (IIFE)
-/// to run function only onse we can use IIFE
-
-(function () {
-  console.log('function has runned');
-  const isPrivat = 23;
-})();
-
-// console.log(isPrivat); // error is not defined
-
-/// arrow variant
-(() => console.log('this function runs once too'))();
-
-{
-  const isPrivat = 23;
-  var notPrivat = 42;
-}
-
-// console.log(isPrivat);
-console.log(notPrivat);
-
-//////// 01.06.2023
-/////// chapter 10 - functions
-
-/// lesson 128 - default parameters
-
-/////adding default parameters
-
-// const bookings = [];
-
-// const bookingFlight = function (
-//   flightNumber,
-//   numberOfPassengers = 3, // this is default number of passengers
-//   price = 50 * numberOfPassengers // this is default price = 50
-// ) {
-//   //earlier es5 setting default values take this way⬇️ lines 17-18
-//   // numberOfPassengers = numberOfPassengers || 15;
-//   // price = price || 50
-
-//   const booking = {
-//     flightNumber,
-//     numberOfPassengers,
-//     price,
+//   return function () {
+//     passengerCount++;
+//     console.log(`there are ${passengerCount} passengers`);
 //   };
-//   console.log(booking);
-//   bookings.push(booking);
 // };
 
-// bookingFlight('123jfk', 15, 54);
-// bookingFlight('123jfk');
-// bookingFlight('123jfk', 16);
-// bookingFlight('123jfk', undefined, 500);
+// const addPassenger = secureBooking();
 
-// console.log(bookings);
+// addPassenger();
+// addPassenger();
+// addPassenger();
 
-// function find(array, element) {
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === element) return i;
-//   }
-//   return 'Not found';
+// console.dir(addPassenger);
+
+// /////// immideately invoked function expression (IIFE)
+// /// to run function only onse we can use IIFE
+
+// (function () {
+//   console.log('function has runned');
+//   const isPrivat = 23;
+// })();
+
+// // console.log(isPrivat); // error is not defined
+
+// /// arrow variant
+// (() => console.log('this function runs once too'))();
+
+// {
+//   const isPrivat = 23;
+//   var notPrivat = 42;
 // }
-///////// 02.06.2023 //////lesson 129
 
-const flight = 'LH238';
-const Andy = {
-  name: 'Andy Fedos',
-  pasport: 375291989,
-};
+// // console.log(isPrivat);
+// console.log(notPrivat);
 
-// const checkIn = function (flightNum, passenger) {
-//   flightNum = 'LH999';
-//   passenger.name = 'Mr.' + passenger.name;
+// //////// 01.06.2023
+// /////// chapter 10 - functions
 
-//   if (passenger.pasport === 375291989) {
-//     alert('Check in!');
-//   } else {
-//     alert('Wrong passport' + passenger.pasport);
-//   }
-//   console.log(flightNum);
+// /// lesson 128 - default parameters
+
+// /////adding default parameters
+
+// // const bookings = [];
+
+// // const bookingFlight = function (
+// //   flightNumber,
+// //   numberOfPassengers = 3, // this is default number of passengers
+// //   price = 50 * numberOfPassengers // this is default price = 50
+// // ) {
+// //   //earlier es5 setting default values take this way⬇️ lines 17-18
+// //   // numberOfPassengers = numberOfPassengers || 15;
+// //   // price = price || 50
+
+// //   const booking = {
+// //     flightNumber,
+// //     numberOfPassengers,
+// //     price,
+// //   };
+// //   console.log(booking);
+// //   bookings.push(booking);
+// // };
+
+// // bookingFlight('123jfk', 15, 54);
+// // bookingFlight('123jfk');
+// // bookingFlight('123jfk', 16);
+// // bookingFlight('123jfk', undefined, 500);
+
+// // console.log(bookings);
+
+// // function find(array, element) {
+// //   for (let i = 0; i < array.length; i++) {
+// //     if (array[i] === element) return i;
+// //   }
+// //   return 'Not found';
+// // }
+// ///////// 02.06.2023 //////lesson 129
+
+// const flight = 'LH238';
+// const Andy = {
+//   name: 'Andy Fedos',
+//   pasport: 375291989,
 // };
 
-// checkIn(flight, Andy);
+// // const checkIn = function (flightNum, passenger) {
+// //   flightNum = 'LH999';
+// //   passenger.name = 'Mr.' + passenger.name;
 
-// console.log(flight); /// flight stays the same like in line 44 - LH238
-// console.log(Andy); /// shown {name: 'Mr.Andy Fedos', pasport: 375291989} - name property has changed - andvlooks like in function mask
+// //   if (passenger.pasport === 375291989) {
+// //     alert('Check in!');
+// //   } else {
+// //     alert('Wrong passport' + passenger.pasport);
+// //   }
+// //   console.log(flightNum);
+// // };
 
-// const flightNum = flight;
-// const passenger = Andy;
-//manipulate with passenger object is the same as manipulate with andy object because they are the same object in the memory heap
+// // checkIn(flight, Andy);
 
-// passing the primitive type in function is the same as a copying, like in line 67
+// // console.log(flight); /// flight stays the same like in line 44 - LH238
+// // console.log(Andy); /// shown {name: 'Mr.Andy Fedos', pasport: 375291989} - name property has changed - andvlooks like in function mask
 
-// const newPassport = function (person) {
-//   person.pasport = Math.trunc(Math.random() * 10000000000);
+// // const flightNum = flight;
+// // const passenger = Andy;
+// //manipulate with passenger object is the same as manipulate with andy object because they are the same object in the memory heap
+
+// // passing the primitive type in function is the same as a copying, like in line 67
+
+// // const newPassport = function (person) {
+// //   person.pasport = Math.trunc(Math.random() * 10000000000);
+// // };
+
+// // newPassport(Andy); /// running of this function changes value pasport property in the object Andy
+
+// // checkIn(flight, Andy); //// and it leads that function checkIn  works incorrect because we get new pasport nuber which is equal to right number
+// // console.log(Andy); //// we get {name: 'Mr.Andy Fedos', pasport: 4325891563} and every function running will give new pasport number in andy object
+
+// ////////////04.06.2023- 05.06.2023
+// ////////callback function
+
+// const oneWord = function (str) {
+//   return str.replaceAll(' ', '').toLowerCase();
+// };
+// // console.log(oneWord('Planet Earth'));
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   // console.log(first, others);
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+// console.log(upperFirstWord('Planet Earth is our home'));
+
+// const transformString = function (str, fn) {
+//   console.log(fn(str));
 // };
 
-// newPassport(Andy); /// running of this function changes value pasport property in the object Andy
+// transformString('all we need is love', oneWord);
+// transformString('all we need is love', upperFirstWord);
 
-// checkIn(flight, Andy); //// and it leads that function checkIn  works incorrect because we get new pasport nuber which is equal to right number
-// console.log(Andy); //// we get {name: 'Mr.Andy Fedos', pasport: 4325891563} and every function running will give new pasport number in andy object
+// // const high5 = function () {
+// //   console.log(`Hello 👋`);
+// // };
 
-////////////04.06.2023- 05.06.2023
-////////callback function
+// // document.body.addEventListener('click', high5); // in this case high5  is a callback fuction which lounched when we click o  a page
 
-const oneWord = function (str) {
-  return str.replaceAll(' ', '').toLowerCase();
-};
-// console.log(oneWord('Planet Earth'));
+// // ['andy', 'mandy', 'semen', 'anton'].forEach(high5);
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  // console.log(first, others);
-  return [first.toUpperCase(), ...others].join(' ');
-};
-console.log(upperFirstWord('Planet Earth is our home'));
+// ////////////lesson 125s
+// /////// funtion returns function
 
-const transformString = function (str, fn) {
-  console.log(fn(str));
-};
-
-transformString('all we need is love', oneWord);
-transformString('all we need is love', upperFirstWord);
-
-// const high5 = function () {
-//   console.log(`Hello 👋`);
+// const greetMe = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting}, ${name}`);
+//   };
 // };
 
-// document.body.addEventListener('click', high5); // in this case high5  is a callback fuction which lounched when we click o  a page
+// greetMe('hi')('pete'); /// one of the variant of writng the function that returns another function
 
-// ['andy', 'mandy', 'semen', 'anton'].forEach(high5);
+// const greetHolla = greetMe('holla');
 
-////////////lesson 125s
-/////// funtion returns function
+// greetHolla('Raul'); ///lines 123-124 also one way to wright function that return  another function
 
-const greetMe = function (greeting) {
-  return function (name) {
-    console.log(`${greeting}, ${name}`);
-  };
-};
+// const greetArrow = greet => firstName => {
+//   console.log(`${greet}, ${firstName}`);
+// };
 
-greetMe('hi')('pete'); /// one of the variant of writng the function that returns another function
+// greetArrow('pryvet')('dude');
 
-const greetHolla = greetMe('holla');
+// ////lesson 134  - call an d apply methods
 
-greetHolla('Raul'); ///lines 123-124 also one way to wright function that return  another function
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
 
-const greetArrow = greet => firstName => {
-  console.log(`${greet}, ${firstName}`);
-};
+//   ///below⬇️ will be a function
 
-greetArrow('pryvet')('dude');
+//   book(flightNum, passengerName) {
+//     console.log(
+//       `${passengerName} booked on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
 
-////lesson 134  - call an d apply methods
+//     this.bookings.push({
+//       flight: `${this.iataCode}${flightNum}`,
+//       passengerName,
+//     });
+//   },
+// };
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
+// lufthansa.book(856, 'andy fedosov');
+// lufthansa.book(253, 'Mandy pedos');
 
-  ///below⬇️ will be a function
+// console.log(lufthansa);
 
-  book(flightNum, passengerName) {
-    console.log(
-      `${passengerName} booked on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
-    this.bookings.push({
-      flight: `${this.iataCode}${flightNum}`,
-      passengerName,
-    });
-  },
-};
+// const book = lufthansa.book;
 
-lufthansa.book(856, 'andy fedosov');
-lufthansa.book(253, 'Mandy pedos');
+// ////to add to this object bookfunction we shouldn't copy it from previous object. because when we copy it we take of it from initial object.
+// // i mean it's bad idea to lounch the book function out of its object.
+// // to solve this problem - we should use three keywords: call, apply, bind
 
-console.log(lufthansa);
+// /////using call keyword
+// book.call(eurowings, 23, 'Sarrah Smith');
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+// console.log(eurowings);
 
-const book = lufthansa.book;
+// /// it looks like we call function book (that in its functionality is the same as lufthansa.book function ) for eurowings object and besides we use 23 as a flightNum , and a sarrah Smith as a passengerName
 
-////to add to this object bookfunction we shouldn't copy it from previous object. because when we copy it we take of it from initial object.
-// i mean it's bad idea to lounch the book function out of its object.
-// to solve this problem - we should use three keywords: call, apply, bind
+// book.call(lufthansa, 149, 'Shimon Zlateck');
+// console.log(lufthansa);
 
-/////using call keyword
-book.call(eurowings, 23, 'Sarrah Smith');
+// const belavia = {
+//   airline: 'Belavia',
+//   iataCode: 'B2',
+//   bookings: [],
+// };
 
-console.log(eurowings);
+// book.call(belavia, 301, 'Andy Fedos');
+// console.log(belavia);
 
-/// it looks like we call function book (that in its functionality is the same as lufthansa.book function ) for eurowings object and besides we use 23 as a flightNum , and a sarrah Smith as a passengerName
+// //// apply method
 
-book.call(lufthansa, 149, 'Shimon Zlateck');
-console.log(lufthansa);
+// const flightData = [444, 'Pete Shmit'];
 
-const belavia = {
-  airline: 'Belavia',
-  iataCode: 'B2',
-  bookings: [],
-};
+// book.apply(belavia, flightData);
 
-book.call(belavia, 301, 'Andy Fedos');
-console.log(belavia);
+// console.log(belavia);
 
-//// apply method
+// //// nowadays the better practise is to use call method and spread operator line 202
 
-const flightData = [444, 'Pete Shmit'];
+// const flightData2 = [258, 'Helen krochek'];
 
-book.apply(belavia, flightData);
+// book.call(eurowings, ...flightData2);
+// console.log(eurowings);
 
-console.log(belavia);
+// ///////////06.06.23
+// ///////// bind method
 
-//// nowadays the better practise is to use call method and spread operator line 202
+// // for the start
+// // book.call(eurowings, 456 'John Smith');
 
-const flightData2 = [258, 'Helen krochek'];
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// const bookB2 = book.bind(belavia);
+// bookEW(23, 'John Smith'); //
 
-book.call(eurowings, ...flightData2);
-console.log(eurowings);
+// const bookEW23 = book.bind(eurowings, 23);
 
-///////////06.06.23
-///////// bind method
+// bookEW23('Misha Fedos');
+// bookEW23('AndyFedos');
 
-// for the start
-// book.call(eurowings, 456 'John Smith');
+// //////using bind Method with event listener
 
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-const bookB2 = book.bind(belavia);
-bookEW(23, 'John Smith'); //
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   this.planes++;
+//   console.log(this.planes);
+// };
 
-const bookEW23 = book.bind(eurowings, 23);
+// // const buyPlaneLH = buyPlane.bind(lufthansa);
+// // lufthansa.buyPlane();
 
-bookEW23('Misha Fedos');
-bookEW23('AndyFedos');
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
-//////using bind Method with event listener
+// //////partial application
 
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  this.planes++;
-  console.log(this.planes);
-};
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.25, 200));
 
-// const buyPlaneLH = buyPlane.bind(lufthansa);
-// lufthansa.buyPlane();
+// const addVAT = addTax.bind(null, 0.2); // it is equal to
+// // addVAT = value + value * 0.2
 
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// console.log(addVAT(300));
 
-//////partial application
+// const addTax2 = function (value) {
+//   return function (rate) {
+//     console.log(value * (1 + rate));
+//   };
+// };
 
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.25, 200));
+// const addTo2K = addTax2(2000);
+// addTo2K(0.3);
 
-const addVAT = addTax.bind(null, 0.2); // it is equal to
-// addVAT = value + value * 0.2
+// const addTax3 = value => rate => console.log(value * (1 + rate));
 
-console.log(addVAT(300));
-
-const addTax2 = function (value) {
-  return function (rate) {
-    console.log(value * (1 + rate));
-  };
-};
-
-const addTo2K = addTax2(2000);
-addTo2K(0.3);
-
-const addTax3 = value => rate => console.log(value * (1 + rate));
-
-addTax3(100)(0.5);
+// addTax3(100)(0.5);
